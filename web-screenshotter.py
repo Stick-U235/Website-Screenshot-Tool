686601c6b9e119c82ddc66f524426bf507c56031
Stick <teardown.thewalls@live.com>
2022-07-22 09:53:28 -0600

    Update web-screenshotter.py

--- web-screenshotter.py
+++ web-screenshotter.py
@@ -79,6 +79,9 @@ def take_screenshot(url):
     driver_path_var = 'chromedriver'
     chrome_options.add_argument('--headless')
     chrome_options.add_argument("--window-size=1920x1080")
+    #below two are incase you're running as root.
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     if args.ignore_ssl:
         print('Ignoring SSL errors')
         chrome_options.add_argument('acceptInsecureCerts')
